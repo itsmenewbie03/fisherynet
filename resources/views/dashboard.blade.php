ef9e97a6670bc2d3c0bfdb86aeda9b401d115688
Toshiven <kevin.abler19@gmail.com>
2024-03-23 09:03:35 +0800

    feat(sidenavbar): add dashboard butto, report

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -49,50 +49,20 @@
                 <ul class="nav">
                     <li class="active">
                         <a href="dashboard.html">
-                            <i class="pe-7s-graph"></i>
+                            <i class="pe-7s-news-paper"></i>
                             <p>Dashboard</p>
                         </a>
                     </li>
                     <li>
                         <a href="user.html">
-                            <i class="pe-7s-user"></i>
-                            <p>User Profile</p>
+                            <i class="pe-7s-graph"></i>
+                            <p>Reports</p>
                         </a>
                     </li>
                     <li>
                         <a href="table.html">
                             <i class="pe-7s-note2"></i>
-                            <p>Table List</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="typography.html">
-                            <i class="pe-7s-news-paper"></i>
-                            <p>Typography</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="icons.html">
-                            <i class="pe-7s-science"></i>
-                            <p>Icons</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="maps.html">
-                            <i class="pe-7s-map-marker"></i>
-                            <p>Maps</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="notifications.html">
-                            <i class="pe-7s-bell"></i>
-                            <p>Notifications</p>
-                        </a>
-                    </li>
-                    <li class="active-pro">
-                        <a href="upgrade.html">
-                            <i class="pe-7s-rocket"></i>
-                            <p>Upgrade to PRO</p>
+                            <p>Configuration</p>
                         </a>
                     </li>
                 </ul>
