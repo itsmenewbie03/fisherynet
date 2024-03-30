682704c78259473350768c36ade063d54f477c14
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-30 19:28:42 +0800

    feat: add handler for config requests

--- app/Console/Commands/MqttSubscribe.php
+++ app/Console/Commands/MqttSubscribe.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Configuration;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use PhpMqtt\Client\Facades\MQTT;
 
@@ -28,9 +30,25 @@ class MqttSubscribe extends Command
     public function handle(): void
     {
         $mqtt = MQTT::connection();
-        $mqtt->subscribe('FISHERYNET|CONFIG_REQUEST', function (string $topic, string $message) {
+        $mqtt->subscribe('FISHERYNET|CONFIG_REQUEST', function (string $topic, string $message) use ($mqtt) {
+            match ($message) {
+                'min_fish_size' => $this->handle_min_fish_size($message),
+                default => Log::info(sprintf('Received QoS level 1 message on topic [%s]: %s', $topic, $message))
+            };
             Log::info(sprintf('Received QoS level 1 message on topic [%s]: %s', $topic, $message));
         }, 0);
+        $mqtt->loop(true, true);
+    }
+
+    public function handle_min_fish_size(string $message): void
+    {
+        $mqtt = MQTT::connection();
+        $fish_size = DB::table('configurations')
+            ->where('key', $message)
+            ->get();
+        $fish_size = $fish_size->value("value");
+        Log::info("Publishing min_fish_size: $fish_size to FISHERYNET|CONFIG_RESPONSE");
+        $mqtt->publish('FISHERYNET|CONFIG_RESPONSE', "min_fish_size=$fish_size", 0, false);
         $mqtt->loop(true, true);
     }
 }
