78fad53b7dd076804e292f7ad141a2011e0b621f
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-30 19:31:08 +0800

    feat: add auto insert config
    This can be further configured to setup default configurations
    automatically during migrations.

--- database/migrations/2024_03_23_025733_create_configurations_table.php
+++ database/migrations/2024_03_23_025733_create_configurations_table.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,6 +17,13 @@ return new class() extends Migration
             $table->text('value');
             $table->timestamps();
         });
+
+        DB::table('configurations')->insert([
+            'key' => 'min_fish_size',
+            'value' => '69',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
