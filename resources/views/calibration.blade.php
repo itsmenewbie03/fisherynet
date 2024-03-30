e14a200e2dee555ab25c3e18e685a71c791ea698
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-03-30 23:44:22 +0800

    feat: add calibration
    This only implements the transfer and retreival of data. The response
    the hardware not reaches the dashboard.

--- resources/views/calibration.blade.php
+++ resources/views/calibration.blade.php
@@ -1,1 +1,9 @@
-<h1>CALIBRATE DEEZ NUTS</h1>
+@if (isset($deeznuts))
+  <p>The value of "deeznuts" is: {{ $deeznuts }}</p>
+@else
+  <p>The "deeznuts" variable is not available.</p>
+@endif
+<form method="POST" action="{{ route('calibrate') }}">
+    @csrf
+    <button type="submit">Set Deez Nuts</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use PhpMqtt\Client\Facades\MQTT;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $deeznuts = session("deeznuts");
+    return view('dashboard', $deeznuts ? ["deeznuts" => $deeznuts] : []);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -22,6 +26,37 @@ Route::middleware('auth')->group(function () {
     Route::resource("/configurations", \App\Http\Controllers\ConfigurationController::class);
     // INFO: reports route
     Route::resource("/reports", \App\Http\Controllers\ReportsController::class);
+
+    Route::post("/calibrate", function () {
+
+        $est_size = -1;
+
+        $mqtt = MQTT::connection();
+        Log::info("STARTING DETECTION CALIBRATION");
+        $mqtt->publish('FISHERYNET|COMMANDS', "START_DETECTION_CALIBRATION", 0, false);
+        $mqtt->loop(true, true);
+
+        $mqtt->subscribe('FISHERYNET|CALIBRATION_RESPONSE', function (string $topic, string $message) use ($mqtt, &$est_size) {
+            Log::info("$topic |  $message");
+            list($key, $value) = explode("=", $message);
+            switch ($key) {
+                case 'est_size':
+                    Log::info("Estimated size from response: $value");
+                    $est_size = $value;
+                    $mqtt->unsubscribe('FISHERYNET|CALIBRATION_RESPONSE');
+                    $mqtt->disconnect();
+                    return;
+                default:
+                    Log::warning("Unexpected response key: $key on topic: $topic");
+            }
+        }, 0);
+
+        $mqtt->loop(true, true);
+        Log::info("Estimated size we will return to dashboard: $est_size");
+        return redirect()->route('dashboard')->with('deeznuts', $est_size);
+
+    })->name('calibrate');
+
 });
 
 require __DIR__ . '/auth.php';
