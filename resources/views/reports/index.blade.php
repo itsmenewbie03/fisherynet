ea32ff2ce49b48a741772c8fde000f9670f0640f
Toshiven <kevin.abler19@gmail.com>
2024-04-04 23:29:31 +0800

    feat(report): remove redundant calandar and add card design

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -1,43 +1,25 @@
 @extends("dashboard")
 
 @section("content")
-<div class="container-fluid mt-4">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <small class="text-muted" id="dateLabel">Default Dates</small>
-            <div id="reportrange" class="form-control" style="cursor: pointer;">
-                <i class="fa fa-calendar"></i>&nbsp;
-                <span></span> <i class="fa fa-caret-down"></i>
+            <div class="card">
+                <div class="card-body">
+                    <div class="input-group">
+                        <small class="text-muted input-group-prepend" id="dateLabel">Report</small>
+                        <div id="reportrange" class="form-control w-100" style="cursor: pointer;">
+                            <i class="fa fa-calendar"></i>&nbsp;
+                            <span></span> <i class="fa fa-caret-down"></i>
+                        </div>
+                    </div>
+                    <div class="mt-3">
+                        <button class="btn btn-success btn-block">Submit</button>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="col-md-6 mt-3 mt-md-0">
-            <small class="text-muted" id="dateLabel">Default Dates</small>
-            <div class="input-group">
-                <input type="text" id="daterange" class="form-control" name="daterange" value="01/01/2018 - 01/15/2018" />
-                <button class="btn btn-outline-secondary" type="button" id="daterangeBtn">
-                    <i class="fas fa-calendar-alt"></i>
-                </button>
-            </div>
-        </div>
-        <div class="col-md-12 mt-3">
-            <button class="btn btn-success btn-block">Submit</button>
-        </div>
-    </div>
-</div>
 @endsection
 
 @push("scripts")
 <script>
     $(function() {
-        $('#daterange').daterangepicker({
-            opens: 'left'
-        }, function(start, end, label) {
-            console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-        });
-    });
-
-    $(function() {
-
         var start = moment().subtract(29, 'days');
         var end = moment();
 
@@ -48,18 +30,18 @@
         $('#reportrange').daterangepicker({
             startDate: start,
             endDate: end,
+            opens: 'left',
             ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
             }
         }, cb);
 
         cb(start, end);
-
     });
 </script>
 @endpush
