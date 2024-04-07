e08f097dd0c07a5010bbaf01b98b4df68f2132e6
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-04-07 22:02:20 +0800

    refactor: auto format using blade formatter

--- resources/views/calibration.blade.php
+++ resources/views/calibration.blade.php
@@ -1,16 +1,16 @@
 @if (isset($est_size))
-  <p>Calibration result: {{ $est_size }}</p>
+    <p>Calibration result: {{ $est_size }}</p>
 @else
-  <p>No calibration result yet.</p>
+    <p>No calibration result yet.</p>
 @endif
 
 <form method="POST" action="{{ route('calibrate') }}">
-  @csrf
-  <button class="btn btn-success" type="submit">Start Calibration</button>
+    @csrf
+    <button class="btn btn-success" type="submit">Start Calibration</button>
 </form>
 
 <form method="POST" action="{{ route('toggle') }}" class="mt-1">
-  @csrf
-  <button class="btn btn-success" type="submit" name="port" value="EXTRA_1">Toggle Extra 1</button>
-  <button class="btn btn-success" type="submit" name="port" value="EXTRA_2">Toggle Extra 2</button>
+    @csrf
+    <button class="btn btn-success" type="submit" name="port" value="EXTRA_1">Toggle Extra 1</button>
+    <button class="btn btn-success" type="submit" name="port" value="EXTRA_2">Toggle Extra 2</button>
 </form>
--- resources/views/configurations/index.blade.php
+++ resources/views/configurations/index.blade.php
@@ -9,88 +9,91 @@ $endDate = date('m/d h:m A', (int) $parts[1]);
 ?>
 @extends('dashboard')
 @section('content')
-  <div class="container-fluid mt-4">
-    @if (session('error'))
-      <div class="alert alert-danger">
-        {{ session('error') }}
-      </div>
-    @endif
-    @if (session('success'))
-      <div class="alert alert-success">
-        {{ session('success') }}
-      </div>
-    @endif
-    <div class="mb-4">
-      <h2 class="text-center">Configure Settings</h2>
-    </div>
-    <table class="table">
-      <form method="POST" action="{{ route('configurations.store') }}">
-        @csrf
-        <tbody>
-          <tr>
-            <td>Enable Sorting</td>
-            <td>
-              <div class="form-floating">
-                <select class="form-control" name="enabled" id="sort">
-                  <option value="1" {{ $is_enabled ? 'selected' : '' }}>Yes</option>
-                  <option value="0" {{ !$is_enabled ? 'selected' : '' }}>No</option>
-                </select>
-              </div>
-              <button class="btn btn-primary btn-block mt-1" name="action_id" value="sorting_toggle"
-                type="submit">Save</button> <!-- Button for the switch -->
-            </td>
-          </tr>
-          <tr>
-            <td>Date Range</td>
-            <td>
-              <div class="form-floating">
-                <input type="text" class="form-control" name="daterange" />
-              </div>
-              <button class="btn btn-primary btn-block mt-1" name="action_id" value="schedule_power_off">Save</button>
-              <!-- Button for date range -->
-            </td>
-          </tr>
-          <tr>
-            <td>Minimum Fish Size</td>
-            <td>
-              <div class="form-floating">
-                <input type="number" class="form-control" id="size" name="min_fish_size" min="1"
-                  value="{{ App\Models\Configuration::all()->where('key', 'min_fish_size')->first()->value ?? 'Key not found' }}" />
-              </div>
-              <button class="btn btn-primary btn-block mt-1" name="action_id" value="update_min_fish_size">Save</button>
-              <!-- Button for area size -->
-            </td>
-          </tr>
-          <tr>
-            <td>Calibration Factor</td>
-            <td>
-              <div class="form-floating">
-                <input type="text" inputmode="decimal" class="form-control" id="cf" name="calibration_factor"
-                  min="1" pattern="[0-9]*[.,]?[0-9]*"
-                  value="{{ App\Models\Configuration::all()->where('key', 'calibration_factor')->first()->value ?? 'Key not found' }}" />
-              </div>
-              <button class="btn btn-primary btn-block mt-1" name="action_id"
-                value="update_calibration_factor">Save</button> <!-- Button for area size -->
-            </td>
-          </tr>
-        </tbody>
-      </form>
-    </table>
+    <div class="container-fluid mt-4">
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+        <div class="mb-4">
+            <h2 class="text-center">Configure Settings</h2>
+        </div>
+        <table class="table">
+            <form method="POST" action="{{ route('configurations.store') }}">
+                @csrf
+                <tbody>
+                    <tr>
+                        <td>Enable Sorting</td>
+                        <td>
+                            <div class="form-floating">
+                                <select class="form-control" name="enabled" id="sort">
+                                    <option value="1" {{ $is_enabled ? 'selected' : '' }}>Yes</option>
+                                    <option value="0" {{ !$is_enabled ? 'selected' : '' }}>No</option>
+                                </select>
+                            </div>
+                            <button class="btn btn-primary btn-block mt-1" name="action_id" value="sorting_toggle"
+                                type="submit">Save</button> <!-- Button for the switch -->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Date Range</td>
+                        <td>
+                            <div class="form-floating">
+                                <input type="text" class="form-control" name="daterange" />
+                            </div>
+                            <button class="btn btn-primary btn-block mt-1" name="action_id"
+                                value="schedule_power_off">Save</button>
+                            <!-- Button for date range -->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Minimum Fish Size</td>
+                        <td>
+                            <div class="form-floating">
+                                <input type="number" class="form-control" id="size" name="min_fish_size"
+                                    min="1"
+                                    value="{{ App\Models\Configuration::all()->where('key', 'min_fish_size')->first()->value ?? 'Key not found' }}" />
+                            </div>
+                            <button class="btn btn-primary btn-block mt-1" name="action_id"
+                                value="update_min_fish_size">Save</button>
+                            <!-- Button for area size -->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Calibration Factor</td>
+                        <td>
+                            <div class="form-floating">
+                                <input type="text" inputmode="decimal" class="form-control" id="cf"
+                                    name="calibration_factor" min="1" pattern="[0-9]*[.,]?[0-9]*"
+                                    value="{{ App\Models\Configuration::all()->where('key', 'calibration_factor')->first()->value ?? 'Key not found' }}" />
+                            </div>
+                            <button class="btn btn-primary btn-block mt-1" name="action_id"
+                                value="update_calibration_factor">Save</button> <!-- Button for area size -->
+                        </td>
+                    </tr>
+                </tbody>
+            </form>
+        </table>
 
-  </div>
+    </div>
 @endsection
 
 @push('scripts')
-  <script>
-    $(function() {
-      $('input[name="daterange"]').daterangepicker({
-        timePicker: true,
-        startDate: '<?php echo $startDate; ?>',
-        endDate: '<?php echo $endDate; ?>',
-        locale: {
-          format: 'M/DD hh:mm A'
-        }
-      });
-    });
-  </script>
+    <script>
+        $(function() {
+            $('input[name="daterange"]').daterangepicker({
+                timePicker: true,
+                startDate: '<?php echo $startDate; ?>',
+                endDate: '<?php echo $endDate; ?>',
+                locale: {
+                    format: 'M/DD hh:mm A'
+                }
+            });
+        });
+    </script>
 @endpush
